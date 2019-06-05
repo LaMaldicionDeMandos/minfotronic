106c3c4317135d422e91d7537d887cd80b3abcd6
Marcelo <marcelo.pasut@etermax.com>
2019-06-05 13:54:42 -0300

    implemento el resto de los colores

--- src/PR_RGB.c
+++ src/PR_RGB.c
@@ -11,6 +11,16 @@
 
 uint8_t* leds = NULL;
 
+typedef union {
+    uint8_t led;
+    struct {
+        uint8_t red:1;
+        uint8_t green:1;
+        uint8_t blue:1;
+        uint8_t empty:5;
+    }bits;
+}led_t;
+
 /***********************************************************************************************************************************
 *** FUNCIONES PRIVADAS AL MODULO
 **********************************************************************************************************************************/
@@ -28,8 +38,14 @@ void LedsRGB( uint8_t led , uint8_t estado ){
         }
         leds = shmat(shmid, NULL, 0);
     }
-    if (led == ROJO) {
-        leds[LED(LED_0)] = estado;
+    led_t ledColor;
+    ledColor.led = leds[LED(LED_0)];
+
+    switch (led) {
+        case ROJO: ledColor.bits.red = estado;break;
+        case VERDE: ledColor.bits.green = estado;break;
+        case AZUL: ledColor.bits.blue = estado;break;
     }
 
+    leds[LED(LED_0)] = ledColor.led;
 }
