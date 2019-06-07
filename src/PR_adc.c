1a556cc1dca73e8c3b2823ece988afe4f4abf4fc
Marcelo <marcelo.pasut@etermax.com>
2019-06-07 14:31:09 -0300

    implemento potenciometro

--- src/PR_adc.c
+++ src/PR_adc.c
@@ -4,37 +4,53 @@
 #include "../inc/PR_adc.h"
 #include "shared_memory.h"
 
-#define THERMOMETER_LOW thermometer_data[15]
-#define THERMOMETER_HIGH thermometer_data[16]
+#define THERMOMETER_LOW adc_data[15]
+#define THERMOMETER_HIGH adc_data[16]
+
+#define POTENTIOMETER_LOW adc_data[17]
+#define POTENTIOMETER_HIGH adc_data[18]
 
 #define ZERO_CELCIUS 2730
-uint8_t* thermometer_data = NULL;
+uint8_t* adc_data = NULL;
 
 typedef union {
     uint16_t value;
     uint8_t  bytes[2];
-} THERMOMETER_TYPE;
+} AD_TYPE;
 
 /***********************************************************************************************************************************
 *** FUNCIONES PRIVADAS AL MODULO
 **********************************************************************************************************************************/
-
-/***********************************************************************************************************************************
-*** FUNCIONES GLOBALES AL MODULO
-**********************************************************************************************************************************/
-int16_t Temperatura() {
+uint8_t* init_data() {
     int shmid;
-    if (!thermometer_data) {
+    if (!adc_data) {
         shmid = init_shared_memory();
         if (shmid < 0) {
             perror("Hubo un error con la libreria infotronic, por favor reinicie.\n");
             return 0;
         }
-        thermometer_data = shmat(shmid, NULL, 0);
+        adc_data = shmat(shmid, NULL, 0);
     }
+    return adc_data;
+}
 
-    THERMOMETER_TYPE thermometer;
-    thermometer.bytes[0] = THERMOMETER_LOW;
-    thermometer.bytes[1] = THERMOMETER_HIGH;
-    return thermometer.value - ZERO_CELCIUS;
+AD_TYPE readData(uint8_t first, uint8_t second) {
+    adc_data = init_data();
+
+    AD_TYPE data;
+    data.bytes[0] = first;
+    data.bytes[1] = second;
+    return data;
+}
+
+
+/***********************************************************************************************************************************
+*** FUNCIONES GLOBALES AL MODULO
+**********************************************************************************************************************************/
+int16_t Temperatura() {
+    return readData(THERMOMETER_LOW, THERMOMETER_HIGH).value - ZERO_CELCIUS;
+}
+
+int16_t Potenciometro ( void ) {
+    return readData(POTENTIOMETER_LOW, POTENTIOMETER_HIGH).value;
 }
